5a85c587992c6e3edb5b6e792e24c5a3ea449e50
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2025-09-27 17:04:36 -0400

    consolidate repeated code

--- demo/lib/demo/content/pages/marker_styling.ex
+++ demo/lib/demo/content/pages/marker_styling.ex
@@ -6,26 +6,5 @@ defmodule DemoWeb.Content.MarkerStyling do
   and the demo app's presentation requirements.
   """
 
-  alias FlyMapEx.Guides.MarkerStyling, as: LibraryGuide
-  alias DemoWeb.Adapters.GuideAdapter
-
-  @doc """
-  Gives the PageLive LiveView the title and description to populate slots in Layouts.app/1,
-  and the live_component to use as a template for rendering the content in this module.
-  """
-
-  def doc_metadata do
-    GuideAdapter.adapt_metadata(LibraryGuide)
-  end
-
-  def tabs do
-    GuideAdapter.adapt_tabs(LibraryGuide)
-  end
-
-  @doc """
-  Delegates to library guide content via adapter.
-  """
-  def get_content(section_key) do
-    GuideAdapter.adapt_content(LibraryGuide, section_key)
-  end
+  use DemoWeb.Content.GenericGuide, guide: FlyMapEx.Guides.MarkerStyling
 end
